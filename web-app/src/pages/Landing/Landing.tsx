bbcce4ad4c603a9cf45d6cf20407600a50fb1985
sergera <sergera88@gmail.com>
2022-05-11 18:38:23 -0300

    Star Notary Landing
    - Added star notary info to landing page

--- web-app/src/pages/Landing/Landing.tsx
+++ web-app/src/pages/Landing/Landing.tsx
@@ -4,10 +4,38 @@ export function Landing() {
 
   return (
     <div className="landing">
-			<h1>Welcome to the Truffle-React dApp Template</h1>
+			<h1>Welcome to the Star Notary dApp</h1>
 
-			<p>With this template you can jump-start the development of your next dApp!</p>
+			<p>Here you can register, name, and negotiate stars!</p>
 
+			<h3>Specifics:</h3>
+			<ul>
+				<li>Every star coordinate and star name must be unique</li>
+				<li>Coordinates use the 
+					<a 						
+						target="_blank" 
+						rel="noopener noreferrer"
+						href="https://en.wikipedia.org/wiki/Equatorial_coordinate_system"
+						className="landing-page__inline-anchor"
+					>
+							Equatorial Coordinate System
+					</a>
+				</li>
+				<li>The only parameters are Right Ascention (RA) and Declination (DEC)</li>
+				<li>RA is measured in hours, minutes, and seconds</li>
+				<li>DEC is measured in degrees, arcminutes, and arcseconds</li>
+				<li>Epoch is always 
+					<a
+						target="_blank" 
+						rel="noopener noreferrer"
+						href="https://en.wikipedia.org/wiki/Epoch_(astronomy)#Julian_dates_and_J2000"
+						className="landing-page__inline-anchor"
+					>
+						J2000.0
+					</a>
+				</li>
+			</ul>
+			
 			<h3>Features:</h3>
 			<ul>
 				<li>Redux store with blockchain-related state management</li>
