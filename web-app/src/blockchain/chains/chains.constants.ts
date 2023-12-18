46f1d14748e411a9d4b1445197ba36247d9d3318
sergio <sergio.joselli@gmail.com>
2023-12-18 16:32:51 -0300

    Fix Indentation

--- web-app/src/blockchain/chains/chains.constants.ts
+++ web-app/src/blockchain/chains/chains.constants.ts
@@ -7,14 +7,13 @@
  * 
  */
 
- import { ChainIdsMap } from './chains.types';
+import { ChainIdsMap } from './chains.types';
 
- export const CHAIN_IDS: ChainIdsMap = {
-	 "mainnet": 1,
-	 "goerli": 5,
-	 "sepolia": 11155111,
-	 // For Development
-	 "truffle": 1337,
-	 "ganache": 5777,
- };
- 
+export const CHAIN_IDS: ChainIdsMap = {
+	"mainnet": 1,
+	"goerli": 5,
+	"sepolia": 11155111,
+	// For Development
+	"truffle": 1337,
+	"ganache": 5777,
+};
